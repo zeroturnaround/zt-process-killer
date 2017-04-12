36334e238b2a4a960d696af82c32784cc0cbe3d1
Raul Valge <raul.valge@zeroturnaround.com>
2017-04-12 15:23:53 +0300

    In case systemroot is not defined, check if most common "C:/windows" path can be used.

--- src/main/java/org/zeroturnaround/process/win/WmicUtil.java
+++ src/main/java/org/zeroturnaround/process/win/WmicUtil.java
@@ -13,8 +13,12 @@ import org.slf4j.LoggerFactory;
  */
 public class WmicUtil {
 
+
   private static final Logger log = LoggerFactory.getLogger(WmicUtil.class);
 
+  private static final String EXECUTABLE_NAME = "WMIC.exe";
+  private static final String DEFAULT_ROOT = "C:/Windows";
+  private static final String DEFAULT_DIR = "System32/Wbem/";
   private static final File PATH = findPath();
 
   public static File getPath() {
@@ -22,22 +26,21 @@ public class WmicUtil {
   }
 
   private static File findPath() {
-    String name = "WMIC.exe";
-    File result = new File(name); // As a fallback use a relative path
+    File path = new File(EXECUTABLE_NAME); // As a fallback use a relative path
     if (SystemUtils.IS_OS_WINDOWS) {
-      String windows = getSystemRoot();
-      if (windows != null) {
-        result = new File(windows, "System32/Wbem/" + name);
+      String rootDir = getSystemRoot();
+      if (rootDir != null) {
+        path = new File(rootDir, DEFAULT_DIR + EXECUTABLE_NAME);
       }
     }
-    log.debug("Using {} at {}", name, result);
-    return result;
+    log.debug("Using {} at {}", EXECUTABLE_NAME, path);
+    return path;
   }
 
   /**
    * Gets the system root environment variable value which on most Windows systems is "C:\Windows".
    *
-   * We have seen that on some machines the variable is written in different cases. Therefor we need to do a case insensitive search.
+   * We have seen that on some machines the variable is written in different cases. Therefore we need to do a case insensitive search.
    */
   private static String getSystemRoot() {
     Map<String, String> envMap = System.getenv();
@@ -46,6 +49,11 @@ public class WmicUtil {
         return envVar.getValue();
       }
     }
+    
+    if (new File(DEFAULT_ROOT).exists()) {
+      return DEFAULT_ROOT;
+    }
+
     return null;
   }
 }
